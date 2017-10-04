2ecc7b9206702233ad5b02f2b6a107ca0f3e582a
Andrew <andrewja314@gmail.com>
2017-10-04 13:59:12 -0400

    Added Use Case Diagram
    Use Case Diagram is still WIP, will finish over the weekend.

--- src/net/mtgsaber/projects/groupprojects/swe3313fall2017/data_handling/customers/Customer.java
+++ src/net/mtgsaber/projects/groupprojects/swe3313fall2017/data_handling/customers/Customer.java
@@ -1,7 +1,8 @@
 package net.mtgsaber.projects.groupprojects.swe3313fall2017.data_handling.customers;
 
 /**
- * Author: Andrew Arnold (9/22/2017)
+ * Represents a customer.
+ * A customer has a name, phone number, address,
  */
 public class Customer {
 }
--- src/net/mtgsaber/projects/groupprojects/swe3313fall2017/receipt/ReceiptPrinter.java
+++ src/net/mtgsaber/projects/groupprojects/swe3313fall2017/receipt/ReceiptPrinter.java
@@ -1,7 +1,7 @@
 package net.mtgsaber.projects.groupprojects.swe3313fall2017.receipt;
 
 /**
- * Author: Andrew Arnold (9/22/2017)
+ * Generates a string representation of a receipt and prints it.
  */
 public class ReceiptPrinter {
 }
