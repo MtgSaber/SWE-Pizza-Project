64817e80f8cc20009c6832af11d759ef76766933
Andrew <andrewja314@gmail.com>
2017-10-30 10:42:37 -0400

    Completed Data Representation
    All of the data classes are complete. All that's left for this package
    is to implement a DBInterface. Might consider adding
    createFromString(String) methods.

--- src/net/mtgsaber/projects/groupprojects/swe3313fall2017/data_handling/Customer.java
+++ src/net/mtgsaber/projects/groupprojects/swe3313fall2017/data_handling/Customer.java
@@ -1,7 +1,44 @@
 package net.mtgsaber.projects.groupprojects.swe3313fall2017.data_handling;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Author: Andrew Arnold (10/23/2017)
  */
 public class Customer {
+    private String name;
+    private ArrayList<Order> orders;
+    private String address;
+    private long id;
+
+    public Customer(String name, String address, long id, Order[] orders) {
+        this.name = name;
+        this.orders = new ArrayList<>(Arrays.asList(orders));
+        this.address = address;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void addOrder(Order order) { orders.add(order); }
+    public Order[] getOrders() {
+        return orders.toArray(new Order[orders.size()]);
+    }
+
+    public long getId() { return id; }
 }
--- src/net/mtgsaber/projects/groupprojects/swe3313fall2017/data_handling/Order.java
+++ src/net/mtgsaber/projects/groupprojects/swe3313fall2017/data_handling/Order.java
@@ -1,8 +1,44 @@
 package net.mtgsaber.projects.groupprojects.swe3313fall2017.data_handling;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Author: Andrew Arnold (10/23/2017)
  */
 public class Order {
+    private final Customer customer;
+    private final User source;
+    private ArrayList<Item> items;
 
+    public Order(Customer customer, User source, Item[] items) {
+        this.customer = customer;
+        this.source = source;
+        this.items = new ArrayList<>(Arrays.asList(items));
+    }
+
+    public Item[] getItems() { return items.toArray(new Item[items.size()]); }
+
+    public void removeItem(int index) throws ArrayIndexOutOfBoundsException {
+        if (index < 0 || index > items.size()-1)
+            throw new ArrayIndexOutOfBoundsException(index);
+        else
+            items.remove(index);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public User getSource() {
+        return source;
+    }
+
+    public BigDecimal getPreTaxTotal() {
+        BigDecimal total = new BigDecimal(0);
+        for (Item item : items)
+            total = total.add(item.getPrice());
+        return total;
+    }
 }
